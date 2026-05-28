aa66fc0f482c3f9f61f8ed8e12bfa85d990723db
aipiao <450752262@qq.com>
2026-05-28 21:20:45 +0800

    设置config

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import httpx
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core import AstrBotConfig
 from astrbot.core.message.components import At
 
 error_code = {403, 404, 429, 500}
 
 
-def getimg(qq, key):
+def getimg(qq, key, api):
+    if api is None:
+        return -1
     url = "https://api.xiaoyu17love.top/API/bqbjh.php"
     params = {
-        "apikey": "ef44b07b17eb4d52268d6619c73fcc604f565c3a2262aee6220bed0125c0d500",
+        "apikey": api,
         "type": "zhitu",
         "key": key,
         "qqs": [qq],
@@ -25,9 +28,11 @@ def getimg(qq, key):
     else:
         return None
 
+
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
@@ -40,6 +45,8 @@ class MyPlugin(Star):
         message = event.get_messages()
         message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
+        config = self.config
+        Key = config.get('Key')
 
         qq_id = sender_id
         for comp in message:
@@ -49,12 +56,15 @@ class MyPlugin(Star):
 
         yield event.plain_result(f'制作中')
 
-        URL = getimg(qq_id, "doro_banging")
+        URL = getimg(qq_id, key="doro_banging", api=Key)
         if URL is None:
             yield event.plain_result(f'制作失败，请到控制台查看详情')
         elif URL in error_code:
             logger.error(URL)
             yield event.plain_result(f'制作失败，请到控制台查看详情')
+        elif URL == -1:
+            logger.warn(f'未填写Key，请前往https://api.xiaoyu17love.top/申请')
+            yield event.plain_result(f'制作失败，未填写APIKey')
         else:
             yield event.image_result(f'{URL}')
 
@@ -65,6 +75,8 @@ class MyPlugin(Star):
         message = event.get_messages()
         message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
+        config = self.config
+        Key = config.get('Key')
 
         qq_id = sender_id
         for comp in message:
@@ -74,12 +86,15 @@ class MyPlugin(Star):
 
         yield event.plain_result(f'制作中')
 
-        URL = getimg(qq_id, key='arona_throw')
+        URL = getimg(qq_id, key='arona_throw', api=Key)
         if URL is None:
             yield event.plain_result(f'制作失败，请到控制台查看详细')
         elif URL in error_code:
             yield event.plain_result(f'制作失败，请到控制台查看详细')
             logger.error(URL)
+        elif URL == -1:
+            logger.warn(f'未填写Key，请前往https://api.xiaoyu17love.top/申请')
+            yield event.plain_result(f'制作失败，未填写APIKey')
         else:
             yield event.image_result(f"{URL}")
 
