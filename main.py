79fff3821cb51c333b64f3968a00810322d61d61
aipiao <450752262@qq.com>
2026-05-29 18:57:09 +0800

    重构代码

--- main.py
+++ main.py
@@ -18,15 +18,21 @@ def getimg(qq, key, api):
         "key": key,
         "qqs": [qq],
     }
-    response = httpx.get(url, params=params)
-    j = response.json()
-    imgUrl = j['data']['url']
-    if j['code'] == 200:
-        return imgUrl
-    elif j['code'] in error_code:
-        return j['code']
+
+    try:
+        response =httpx.get(url, params=params)
+        data = response.json()
+    except httpx.HTTPStatusError as e:
+        return f"API 服务异常 (HTTP {e.response.status_code})"
+
+    code = data['code']
+    if code == 200:
+        img_url = data['data']['url']
+        return img_url
+    elif code in error_code:
+        return f'制作失败，错误代码：{code}'
     else:
-        return None
+        return '制作失败，原因未知'
 
 
 class MyPlugin(Star):
@@ -37,16 +43,13 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("doro")
-    async def doro(self, event: AstrMessageEvent):
-        """生成doro敲头"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    def _hand_image_command(self, event: AstrMessageEvent, key: str):
         sender_id = event.get_sender_id()
         message = event.get_messages()
         message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         config = self.config
-        Key = config.get('Key')
+        api = config.get('Key')
 
         qq_id = sender_id
         for comp in message:
@@ -54,49 +57,29 @@ class MyPlugin(Star):
                 qq_id = comp.qq
                 break
 
-        yield event.plain_result(f'制作中')
+        result = getimg(qq_id, key, api)
+        return result
 
-        URL = getimg(qq_id, key="doro_banging", api=Key)
-        if URL is None:
-            yield event.plain_result(f'制作失败，请到控制台查看详情')
-        elif URL in error_code:
-            logger.error(URL)
-            yield event.plain_result(f'制作失败，请到控制台查看详情')
-        elif URL == -1:
-            logger.warn(f'未填写Key，请前往https://api.xiaoyu17love.top/申请')
-            yield event.plain_result(f'制作失败，未填写APIKey')
+
+    @filter.command("doro")
+    async def doro(self, event: AstrMessageEvent):
+        """生成doro敲头"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        yield event.plain_result(f'制作中')
+        img_url = self._hand_image_command(event,key="arona_throw")
+        if 'http' in img_url:
+            yield event.image_result(f'{img_url}')
         else:
-            yield event.image_result(f'{URL}')
+            yield event.plain_result(f'{img_url}')
 
     @filter.command('arona')
     async def arona(self, event: AstrMessageEvent):
         """生成阿罗娜丢"""
-        sender_id = event.get_sender_id()
-        message = event.get_messages()
-        message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        config = self.config
-        Key = config.get('Key')
-
-        qq_id = sender_id
-        for comp in message:
-            if isinstance(comp, At):
-                qq_id = comp.qq
-                break
-
         yield event.plain_result(f'制作中')
-
-        URL = getimg(qq_id, key='arona_throw', api=Key)
-        if URL is None:
-            yield event.plain_result(f'制作失败，请到控制台查看详细')
-        elif URL in error_code:
-            yield event.plain_result(f'制作失败，请到控制台查看详细')
-            logger.error(URL)
-        elif URL == -1:
-            logger.warn(f'未填写Key，请前往https://api.xiaoyu17love.top/申请')
-            yield event.plain_result(f'制作失败，未填写APIKey')
+        img_url = self._hand_image_command(event, key="doro_banging")
+        if 'http' in img_url:
+            yield event.image_result(f'{img_url}')
         else:
-            yield event.image_result(f"{URL}")
+            yield event.plain_result(f'{img_url}')
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
